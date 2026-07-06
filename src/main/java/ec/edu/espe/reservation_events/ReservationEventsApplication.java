2636da3036ebd1bab2f5509e0e3cd80facdc5213
tufita2003 <tufita2003@gmail.com>
2026-07-06 15:13:08 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/ec/edu/espe/reservation_events/ReservationEventsApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.reservation_events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/reservation_events/ReservationEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.reservation_events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
